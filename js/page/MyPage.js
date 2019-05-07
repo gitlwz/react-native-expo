759b99f8d7cabdc10d2ce8bbd34a107af749a094
liuwenzhu <547485261@qq.com>
2019-05-07 09:38:06 +0800

    我的页面

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -1,45 +1,98 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import actions from '../action/index';
 import NavigationUtil from "../navigator/NavigationUtil";
+import NavigationBar from '../common/NavigationBar';
+import { MORE_MENU } from "../common/MORE_MENU";
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import GlobalStyles from "../res/GlobalStyles";
+import ViewUtil from "../util/ViewUtil";
+const THEME_COLOR = '#678';
 
 class MyPage extends React.Component {
-    _changeTheme = () => {
-        const { onThemeChange } = this.props;
-        onThemeChange("red")
+    onClick = (menu) => {
+
     }
-    _onPress = () => {
-        NavigationUtil.goPage({}, "DetailPage")
-    }
-    _goFetch = () => {
-        NavigationUtil.goPage({}, "FetchDemoPage")
-    }
-    _goFetch2 = () => {
-        NavigationUtil.goPage({}, "AsyncStorageDemoPage")
-    }
-    _goDataStoreDemoPage = () => {
-        NavigationUtil.goPage({}, "DataStoreDemoPage")
-    }
-    _clear = () => {
-        AsyncStorage.clear();
+    getItem = (menu) => {
+        return ViewUtil.getMenuItem(
+            this.onClick(menu),
+            menu,
+            THEME_COLOR
+        )
     }
     render() {
+        let statusBar = {
+            backgroundColor: THEME_COLOR,
+            barStyle: 'light-content',
+        };
+        let navigationBar =
+            <NavigationBar
+                title={'我的'}
+                statusBar={statusBar}
+                style={{ backgroundColor: THEME_COLOR }}
+            />;
         return (
-            <View style={styles.container}>
-                <Text>MyPage</Text>
-                <Button
-                    title="改变主题颜色"
-                    onPress={this._changeTheme}
-                />
-                <Button
-                    title="清除所有缓存"
-                    onPress={this._clear}
-                />
-                <Text onPress={this._onPress}>跳转到详情页</Text>
-                <Text onPress={this._goFetch}>Fetch 使用</Text>
-                <Text onPress={this._goFetch2}>AsyncStorageDemoPage 使用</Text>
-                <Text onPress={this._goDataStoreDemoPage}>DataStoreDemoPage 使用</Text>
+            <View style={GlobalStyles.root_container}>
+                {navigationBar}
+                <ScrollView>
+                    <TouchableOpacity
+                        style={styles.item}
+                        onPress={() => this.onClick(MORE_MENU.About)}
+                    >
+                        <View style={styles.about_left}>
+                            <Ionicons
+                                name={MORE_MENU.About.icon}
+                                size={40}
+                                style={{
+                                    marginRight: 10,
+                                    color: THEME_COLOR,
+                                }}
+                            />
+                            <Text>GitHub Popular</Text>
+                        </View>
+                        <Ionicons
+                            name={'ios-arrow-forward'}
+                            size={16}
+                            style={{
+                                marginRight: 10,
+                                alignSelf: 'center',
+                                color: THEME_COLOR,
+                            }} />
+                    </TouchableOpacity>
+                    <View style={GlobalStyles.line} />
+                    {this.getItem(MORE_MENU.Tutorial)}
+                    <Text style={styles.groupTitle}>趋势管理</Text>
+                    {/*自定义语言*/}
+                    {this.getItem(MORE_MENU.Custom_Language)}
+                    {/*语言排序*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Sort_Language)}
+
+                    {/*最热管理*/}
+                    <Text style={styles.groupTitle}>最热管理</Text>
+                    {/*自定义标签*/}
+                    {this.getItem(MORE_MENU.Custom_Key)}
+                    {/*标签排序*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Sort_Key)}
+                    {/*标签移除*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.Remove_Key)}
+
+                    {/*设置*/}
+                    <Text style={styles.groupTitle}>设置</Text>
+                    {/*自定义主题*/}
+                    {this.getItem(MORE_MENU.Custom_Theme)}
+                    {/*关于作者*/}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.About_Author)}
+                    <View style={GlobalStyles.line}/>
+                    {/*反馈*/}
+                    {this.getItem(MORE_MENU.Feedback)}
+                    <View style={GlobalStyles.line}/>
+                    {this.getItem(MORE_MENU.CodePush)}
+                </ScrollView>
             </View>
         );
     }
@@ -48,10 +101,27 @@ class MyPage extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#f3f3f4',
     },
+    about_left: {
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    item: {
+        backgroundColor: 'white',
+        padding: 10,
+        height: 90,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+    groupTitle: {
+        marginLeft: 10,
+        marginTop: 10,
+        marginBottom: 5,
+        fontSize: 12,
+        color: 'gray'
+    }
 });
 
 const mapStateToProps = state => ({});
